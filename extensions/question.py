744540a02f3246cdfe1818627708e93daf20c098
Gstar <gstar973@outlook.fr>
2023-07-23 01:41:02 +0200

    Commit

--- extensions/question.py
+++ extensions/question.py
@@ -1,6 +1,6 @@
 import asyncio
 from discord.ext import commands
-from constants import QUESTION_BOT_CHANNEL_ID, ADMIN_ROLE_ID, OTHER_ROLE_ID, AUTHOR_ROLE_ID, GSTAR_ROLE_ID
+from constants import QUESTION_BOT_CHANNEL_ID, ADMIN_ROLE_ID, OTHER_ROLE_ID, AUTHOR_ROLE_ID
 
 class Question(commands.Cog):
     def __init__(self, bot):
@@ -14,7 +14,7 @@ class Question(commands.Cog):
                 response = await self.bot.wait_for('message', check=check, timeout=60)
                 return response
             except asyncio.TimeoutError:
-                await thread.send(f"{thread.owner.mention} Je n'ai pas compris votre réponse. Veuillez répondre par 'Oui' ou 'Non'.")
+                await thread.send(f"{thread.owner.mention} Je n'ai pas reçu de réponse. Veuillez répondre par 'Oui' ou 'Non'.")
 
     @commands.Cog.listener()
     async def on_thread_join(self, thread):
@@ -33,7 +33,7 @@ class Question(commands.Cog):
                     await message.author.send("Vous n'êtes pas autorisé à écrire dans ce fil.")
                     await message.delete()
             except Exception as e:
-                print(f"Une erreur est survenue lors de la suppression d'un message non autorisé : {e}")
+                print(f"Une erreur est survenue lors de la gestion des messages dans le fil : {e}")
 
     @commands.Cog.listener()
     async def on_thread_create(self, thread):
@@ -53,12 +53,9 @@ class Question(commands.Cog):
             await thread.send(f"{thread.owner.mention} Très bien, votre titre semble approprié. Le fil est maintenant ouvert à tous pour la discussion. Merci de votre coopération.")
             # Retirer le rôle AUTHOR_ROLE_ID de l'auteur du fil
             await thread.owner.remove_roles(author_role)
-            # Ajouter le rôle OTHER_ROLE_ID à tous les autres membres
+            # Donner la permission de parler aux membres ayant le rôle OTHER_ROLE_ID
             other_role = thread.guild.get_role(OTHER_ROLE_ID)
-            gstar_role = thread.guild.get_role(GSTAR_ROLE_ID)
-            for member in thread.guild.members:
-                if other_role not in member.roles and gstar_role not in member.roles:
-                    await member.add_roles(other_role)
+            await thread.set_permissions(other_role, send_messages=True)
         elif response.content.lower() == 'non':
             while True:
                 new_title = await self.ask_question(thread, "Merci de reformuler votre titre en une question compréhensible et détaillée. Par exemple, au lieu de 'Conseil stuff', vous pourriez dire 'Quelles améliorations puis-je apporter à mon stuff ?' Ecrivez donc à la suite de ce message votre nouveau titre.", check)
@@ -73,15 +70,15 @@ class Question(commands.Cog):
                     await thread.send(f"{thread.owner.mention} Votre titre a bien été modifié. Le fil est maintenant ouvert à tous pour la discussion. Merci de votre coopération.")
                     # Retirer le rôle AUTHOR_ROLE_ID de l'auteur du fil
                     await thread.owner.remove_roles(author_role)
-                    # Ajouter le rôle OTHER_ROLE_ID à tous les autres membres
+                    # Donner la permission de parler aux membres ayant le rôle OTHER_ROLE_ID
                     other_role = thread.guild.get_role(OTHER_ROLE_ID)
-                    gstar_role = thread.guild.get_role(GSTAR_ROLE_ID)
-                    for member in thread.guild.members:
-                        if other_role not in member.roles and gstar_role not in member.roles:
-                            await member.add_roles(other_role)
+                    await thread.set_permissions(other_role, send_messages=True)
                 except Exception as e:
                     print(f"Erreur lors de la modification du titre du fil : {e}")
                 break
+        else:
+            await thread.send(f"{thread.owner.mention} Je n'ai pas compris votre réponse.")
+            await self.on_thread_create(thread)
 
 async def setup(bot):
     await bot.add_cog(Question(bot))
