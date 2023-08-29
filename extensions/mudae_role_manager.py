4ea40da162d76aa8c005074c60f84d52488b9af4
Gstar <gstar973@outlook.fr>
2023-08-29 22:07:14 +0200

    commit

--- extensions/mudae_role_manager.py
+++ extensions/mudae_role_manager.py
@@ -6,8 +6,7 @@ from constants import *
 
 TIMEOUT_DURATION = 5
 
-# Liste des commandes acceptées
-SLASH_COMMANDS = ["wa", "ha", "ma", "wg", "hg", "mg"]
+SLASH_COMMANDS = ["/wa", "/ha", "/ma", "/wg", "/hg", "/mg"]
 TEXT_COMMANDS = ["$wa", "$ha", "$ma", "$wg", "$hg", "$mg", "$rolls", "$bw", "$bk", "$tu", "$mu", "$ku", "$rt", "$vote", "$daily"]
 
 class MudaeRoleManager(commands.Cog):
@@ -27,74 +26,64 @@ class MudaeRoleManager(commands.Cog):
     async def on_message(self, message):
         guild = self.bot.get_guild(GUILD_ID_TEST)
         if guild is None:
+            print("Bot is not in the guild.")
             return
 
-        if not message.author.bot and message.channel.id == GENERAL_CHANNEL_ID:
-            if message.content in TEXT_COMMANDS:
-                await self.manage_roles(message)
+        # Check if the message is a slash command
+        if message.application_id is not None and message.interaction is not None:
+            command_name = message.interaction.name  # Get the name of the slash command
 
-    @commands.Cog.listener()
-    async def on_interaction(self, interaction):
-        if interaction.type == discord.InteractionType.application_command:
-            command = interaction.data["name"]
-            if command in SLASH_COMMANDS:
-                await self.manage_roles(interaction)
+            # Check if the command is in the list of allowed slash commands
+            if f"/{command_name}" in SLASH_COMMANDS:
+                user = message.interaction.user  # Get the user who invoked the slash command
 
-    async def manage_roles(self, event):
+                # Log the details
+                print(f"Slash command detected: {command_name}")
+                print(f"User who invoked the command: {user.name}#{user.discriminator}")
+
+        elif not message.author.bot and message.channel.id == GENERAL_CHANNEL_ID:
+            if message.content in SLASH_COMMANDS or message.content in TEXT_COMMANDS:
+                role_membre_test = discord.utils.get(guild.roles, id=MEMBRE_TEST_ID)
+                role_singe_mudae = discord.utils.get(guild.roles, id=SINGE_MUDAE_ID)
+
+                if role_membre_test is None:
+                    print("MEMBRE_TEST role not found.")
+                    return
+
+                if role_singe_mudae is None:
+                    print("SINGE_MUDAE role not found.")
+                    return
+
+                channel = guild.get_channel(GENERAL_CHANNEL_ID)
+                if channel is None:
+                    print("Channel not found.")
+                    return
+
+                await message.author.add_roles(role_singe_mudae)
+                await channel.set_permissions(role_membre_test, send_messages=False)
+                await channel.set_permissions(message.author, send_messages=True)
+
+                self.user_timeout[message.author.id] = datetime.now() + timedelta(seconds=TIMEOUT_DURATION)
+
+                asyncio.create_task(self.reset_role_and_channel_permissions(message.author.id))
+
+    async def reset_role_and_channel_permissions(self, user_id):
+        await asyncio.sleep(TIMEOUT_DURATION)
+
         guild = self.bot.get_guild(GUILD_ID_TEST)
-        role_membre_test = guild.get_role(MEMBRE_TEST_ID)
-        role_singe_mudae = guild.get_role(SINGE_MUDAE_ID)
-
-        if role_membre_test is None or role_singe_mudae is None:
-            print("Roles not found.")
-            return
-        
+        role_membre_test = discord.utils.get(guild.roles, id=MEMBRE_TEST_ID)
+        role_singe_mudae = discord.utils.get(guild.roles, id=SINGE_MUDAE_ID)
         channel = guild.get_channel(GENERAL_CHANNEL_ID)
-        if channel is None:
-            print("Channel not found.")
+        user = guild.get_member(user_id)
+
+        if role_membre_test is None or role_singe_mudae is None or channel is None or user is None:
             return
 
-        # Donne le rôle SINGE_MUDAE à l'utilisateur
-        await event.author.add_roles(role_singe_mudae)
+        await user.remove_roles(role_singe_mudae)
+        await channel.set_permissions(role_membre_test, send_messages=True)
+        await channel.set_permissions(user, send_messages=None)
 
-        # Met à jour les permissions du canal
-        await channel.set_permissions(role_membre_test, send_messages=False)
-        await channel.set_permissions(event.author, send_messages=True)
-
-        # Met à jour ou définit le délai d'expiration pour l'utilisateur
-        self.user_timeout[event.author.id] = datetime.now() + timedelta(seconds=TIMEOUT_DURATION)
-
-        # Lance la tâche de réinitialisation du rôle
-        self.reset_role_and_channel_permissions.start()
-
-    @tasks.loop(seconds=1)
-    async def reset_role_and_channel_permissions(self):
-        current_time = datetime.now()
-        guild = self.bot.get_guild(GUILD_ID_TEST)
-        
-        role_membre_test = guild.get_role(MEMBRE_TEST_ID)
-        role_singe_mudae = guild.get_role(SINGE_MUDAE_ID)
-
-        if role_membre_test is None or role_singe_mudae is None:
-            return
-
-        channel = guild.get_channel(GENERAL_CHANNEL_ID)
-        if channel is None:
-            return
-
-        for user_id, timeout in list(self.user_timeout.items()):
-            user = guild.get_member(user_id)
-
-            if current_time >= timeout:
-                # Retire le rôle SINGE_MUDAE de l'utilisateur
-                await user.remove_roles(role_singe_mudae)
-
-                # Réinitialise les permissions du canal
-                await channel.set_permissions(role_membre_test, send_messages=True)
-                await channel.set_permissions(user, send_messages=None)
-
-                # Retire l'utilisateur du dictionnaire de délai d'expiration
-                del self.user_timeout[user_id]
+        del self.user_timeout[user_id]
 
 async def setup(bot):
     await bot.add_cog(MudaeRoleManager(bot))
