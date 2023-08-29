109969019d07f5dae85340966783d9804fa33364
Gstarmix <gstar973@outlook.fr>
2023-08-30 01:28:04 +0200

    commit

--- bot.py
+++ bot.py
@@ -19,10 +19,7 @@ async def on_ready():
     await bot.load_extension("extensions.presentation")
     await bot.load_extension("extensions.question")
     await bot.load_extension("extensions.command_check")
-
-    # await bot.load_extension("extensions.emoji_changer")
-
-    # await bot.load_extension("extensions.channel_visibility_manager")
-    await bot.load_extension("extensions.mudae_role_manager")
+    await bot.load_extension("extensions.emoji_changer")
+    await bot.load_extension("extensions.channel_visibility_manager")
 
 bot.run(TOKEN)
