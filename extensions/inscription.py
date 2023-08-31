85e9c1c8f23cfb968ebf653325fafa3b81f8d125
Gstar <gstar973@outlook.fr>
2023-08-31 08:20:01 +0200

    commit

--- extensions/inscription.py
+++ extensions/inscription.py
@@ -61,7 +61,9 @@ class Inscription(commands.Cog):
             thread = await message.create_thread(name=f"Inscription de {message.author.name}")
             self.threads[thread.id] = message.author.id
             self.threads_created.add(message.id)
-            message_content = "Merci de votre intérêt pour le concours ! Êtes-vous un joueur de NosTale? Si oui, partagez une capture d'écran de votre personnage in-game, en veillant à ce que le pseudo, la date et l'heure soient visibles. Si vous n'êtes pas un joueur de NosTale, veuillez le préciser."
+            message_content = ("Merci de votre intérêt pour le concours ! Pour poursuivre votre inscription, veuillez suivre les instructions suivantes :\n"
+                            "- Si vous êtes un joueur de NosTale, partagez ici une capture d'écran de votre personnage in-game en vous assurant que le pseudo, la date et l'heure soient bien visibles.\n"
+                            "- Si vous n'êtes pas un joueur de NosTale, répondez simplement à ce message en écrivant la phrase exacte `Je ne suis pas un joueur de NosTale`.")
         except:
             return
         try:
