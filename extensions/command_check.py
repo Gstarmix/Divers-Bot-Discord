39bc59f8107c8aeaf091f48f816b4ffca7c8ee8c
Gstar <gstar973@outlook.fr>
2023-09-01 02:27:23 +0200

    commit

--- extensions/command_check.py
+++ extensions/command_check.py
@@ -15,24 +15,20 @@ class CommandCheck(commands.Cog):
             MULTI_GAMES_CHANNEL_ID: ["$blacktea", "$greentea", "$redtea", "$yellowtea", "$mixtea", "$quiz", "$jankenpon", "$pokeduel", "/bingo", "/chifumi", "/colormind", "/jeux", "/morpion", "/pendu", "/puissance4", "$42ball", "$mm", "$pr", "$profile", "$ima", "$im", "$search", "$changeimg", "$fate", "$quotimage", "$beam", "$blacktea", "$note", "$fn", "$sm", "$firstmarry", "$fm", "$al", "$alias", "$a2", "$top", "$topo", "$topl", "$topserv", "$topservk", "$tsk", "$left", "$myid", "$avatar", "$skills", "$embedcolor", "$help", "$divorce", "$skillsgm", "$vsgm", "$dsgm", "$favarenagm", "$bonus"],
             MUDAE_MODO_CHANNEL_ID: [],
             LOG_CHANNEL_ID: [],
-            MUDAE_WAIFUS_CHANNEL_2_ID: [],
+            MUDAE_CONTROL_CHANNEL_ID: [],
+            MUDAE_WAIFUS_CHANNEL_2_ID: ["$waifu", "$waifua", "$waifug", "$waifub", "$husbando", "$husbandoa", "$husbandog", "$husbandob", "$marry", "$marrya", "$marryg", "$marryb", "$w", "$h", "$m", "$wa", "$ha", "$ma", "$wg", "$hg", "$mg", "$us", "$usestack", "$tu", "$timersup", "$mu", "$marryup", "$ku", "$kakeraup", "$vote", "$daily", "$rolls", "$dk", "$dailykakera"],
             MUDAE_SETTINGS_CHANNEL_2_ID: []
         }
 
-        for accomplishment_channel_id in ACCOMPLISSEMENT_CHANNEL_ID:
-            self.allowed_commands[accomplishment_channel_id] = []
-
-        self.allowed_commands[MUDAE_WAIFUS_CHANNEL_2_ID] = ["$w", "$h", "$m", "$wa", "$ha", "$ma", "$wg", "$hg", "$mg", "$us", "$tu", "$mu", "$ku", "$vote", "$daily", "$rolls", "$dk"]
-
+        self.forbidden_commands = ["$lang", "$skiptuto", "$settings", "$setrare", "$settimer", "$setrolls", "$setclaim", "$shifthour", "$setinterval", "$haremlimit", "$togglereact", "$channelinstance", "$gamemode", "$servlimroul", "$togglebuttons", "$toggleclaimrolls", "$togglelikerolls", "$togglekakerarolls", "$togglehentai", "$toggledisturbing", "$toggleclaimrank", "$togglelikerank", "$serverdisable", "$togglesnipe", "$togglekakerasnipe", "$leftusers", "$restorelist", "$restore", "$channeldeny", "$channelrestrict", "$setchannel", "$restrict", "$deny", "$setpermission", "$togglesilent", "$givecustom", "$forcedivorce", "$cleanuser", "$userdivorce", "$thanos", "$thanosall", "$bitesthedust", "$clearnotes", "$clearwishes", "$resetalias2", "$fullreset", "$mk", "$togglekakera", "$badgevalue", "$cleankakera", "$givescrap", "$kakerascrap", "$addimg", "$addcustom", "$claimreact", "$kakerareact", "$wishseries", "$haremcopy", "$kakeracopy", "$limroul", "$setpermission", "$ic", "$togglekakerarolls", "$toggleclaimrolls", "$togglelikerolls"]
+        
         self.mod_commands = []
         for channel_id, commands in self.allowed_commands.items():
-            if channel_id not in [MUDAE_MODO_CHANNEL_ID, LOG_CHANNEL_ID, MUDAE_CONTROL_CHANNEL_ID, MUDAE_WAIFUS_CHANNEL_2_ID, MUDAE_SETTINGS_CHANNEL_2_ID] + ACCOMPLISSEMENT_CHANNEL_ID:
+            if channel_id not in [MUDAE_MODO_CHANNEL_ID, LOG_CHANNEL_ID, MUDAE_CONTROL_CHANNEL_ID, MUDAE_WAIFUS_CHANNEL_2_ID, MUDAE_SETTINGS_CHANNEL_2_ID]:
                 self.mod_commands.extend(commands)
 
         all_commands_except_poke_and_waifus = set(self.mod_commands) - set(self.allowed_commands[MUDAE_POKESLOT_CHANNEL_ID]) - set(self.allowed_commands[MUDAE_WAIFUS_CHANNEL_2_ID])
         self.allowed_commands[MUDAE_SETTINGS_CHANNEL_2_ID] = list(all_commands_except_poke_and_waifus)
-
-        self.forbidden_commands = ["$lang", "$skiptuto", "$settings", "$setrare", "$settimer", "$setrolls", "$setclaim", "$shifthour", "$setinterval", "$haremlimit", "$togglereact", "$channelinstance", "$gamemode", "$servlimroul", "$togglebuttons", "$toggleclaimrolls", "$togglelikerolls", "$togglekakerarolls", "$togglehentai", "$toggledisturbing", "$toggleclaimrank", "$togglelikerank", "$serverdisable", "$togglesnipe", "$togglekakerasnipe", "$leftusers", "$restorelist", "$restore", "$channeldeny", "$channelrestrict", "$setchannel", "$restrict", "$deny", "$setpermission", "$togglesilent", "$givecustom", "$forcedivorce", "$cleanuser", "$userdivorce", "$thanos", "$thanosall", "$bitesthedust", "$clearnotes", "$clearwishes", "$resetalias2", "$fullreset"]
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -40,36 +36,50 @@ class CommandCheck(commands.Cog):
             return
 
         split_message = message.content.split()
-        if split_message:
-            command = split_message[0]
-        else:
-            command = ""
+        command = split_message[0] if split_message else ""
 
         if command in self.forbidden_commands:
-            if any(role.id in [CHEF_SINGE_ROLE_ID, MUDAE_MODO_ROLE_ID, MUDAE_CONTROL_CHANNEL_ID] for role in message.author.roles):
-                return
-            await message.delete()
-            await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande admin `{command}`. Cette commande est réservée à l'administrateur et aux modérateurs. Je vous prie de ne pas l'utiliser.")
+            if not any(role.id in [CHEF_SINGE_ROLE_ID, MUDAE_MODO_ROLE_ID] for role in message.author.roles):
+                await message.delete()
+                await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande admin `{command}`. Cette commande est réservée à l'administrateur et aux modérateurs. Je vous prie de ne pas l'utiliser.")
             return
 
         if command.startswith('$') or command.startswith('/'):
             if command not in self.mod_commands:
                 return
-            if command in self.mod_commands and (message.channel.id in [MUDAE_MODO_CHANNEL_ID, LOG_CHANNEL_ID, MUDAE_CONTROL_CHANNEL_ID, MUDAE_WAIFUS_CHANNEL_2_ID, MUDAE_SETTINGS_CHANNEL_2_ID] or message.channel.id in ACCOMPLISSEMENT_CHANNEL_ID):
+
+            if message.channel.id in [MUDAE_MODO_CHANNEL_ID, LOG_CHANNEL_ID, MUDAE_CONTROL_CHANNEL_ID]:
                 return
-            allowed_channels = []
-            for channel_id, commands in self.allowed_commands.items():
-                if command in commands:
-                    allowed_channels.append(channel_id)
-            if message.channel.id not in allowed_channels:
-                content = message.content
-                await message.delete()
-                allowed_channels_str = ', '.join([f"<#{channel_id}>" for channel_id in allowed_channels if channel_id not in [MUDAE_MODO_CHANNEL_ID, LOG_CHANNEL_ID, MUDAE_CONTROL_CHANNEL_ID, MUDAE_WAIFUS_CHANNEL_2_ID, MUDAE_SETTINGS_CHANNEL_2_ID] + ACCOMPLISSEMENT_CHANNEL_ID])
-                if message.channel.id == MUDAE_TUTORIAL_CHANNEL_ID:
-                    await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande `{content}` dans le mauvais salon. Veuillez l'envoyer dans le bon salon : {allowed_channels_str}. Une fois cela effectué, veuillez rafraîchir le tutoriel en tapant à nouveau `$tuto` dans ce salon.")
+
+            if message.channel.id == MUDAE_WAIFUS_CHANNEL_2_ID:
+                if command in self.allowed_commands[MUDAE_POKESLOT_CHANNEL_ID]:
+                    await message.delete()
+                    await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande `{command}` dans le mauvais salon. Veuillez l'envoyer dans le bon salon : <#{MUDAE_POKESLOT_CHANNEL_ID}>.")
+                    return
+                if command not in self.allowed_commands[MUDAE_WAIFUS_CHANNEL_2_ID]:
+                    await message.delete()
+                    await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande `{command}` dans le mauvais salon. Veuillez l'envoyer dans le bon salon : <#{MUDAE_SETTINGS_CHANNEL_2_ID}>.")
+                    return
+
+            if message.channel.id == MUDAE_SETTINGS_CHANNEL_2_ID:
+                if command in self.allowed_commands[MUDAE_POKESLOT_CHANNEL_ID] or command in self.allowed_commands[MUDAE_WAIFUS_CHANNEL_2_ID] or command in self.forbidden_commands:
+                    await message.delete()
+                    target_channel = MUDAE_POKESLOT_CHANNEL_ID if command in self.allowed_commands[MUDAE_POKESLOT_CHANNEL_ID] else MUDAE_WAIFUS_CHANNEL_2_ID
+                    await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande `{command}` dans le mauvais salon. Veuillez l'envoyer dans le bon salon : <#{target_channel}>.")
+                    return
                 else:
-                    await message.channel.send(f"{message.author.mention} Vous avez envoyé la commande `{content}` dans le mauvais salon. Veuillez l'envoyer dans le bon salon : {allowed_channels_str}.")
-                return
+                    return
+
+            allowed_channels = [channel_id for channel_id, commands in self.allowed_commands.items() if command in commands]
+            allowed_channels = list(filter(lambda x: x not in [MUDAE_MODO_CHANNEL_ID, LOG_CHANNEL_ID, MUDAE_CONTROL_CHANNEL_ID, MUDAE_SETTINGS_CHANNEL_2_ID], allowed_channels))
+            allowed_channels_str = ', '.join([f"<#{channel_id}>" for channel_id in allowed_channels])
+
+            if message.channel.id not in allowed_channels:
+                await message.delete()
+                wrong_channel_msg = f"{message.author.mention} Vous avez envoyé la commande `{command}` dans le mauvais salon. Veuillez l'envoyer dans le bon salon : {allowed_channels_str}."
+                if message.channel.id == MUDAE_TUTORIAL_CHANNEL_ID:
+                    wrong_channel_msg += " Une fois cela effectué, veuillez rafraîchir le tutoriel en tapant à nouveau `$tuto` dans ce salon."
+                await message.channel.send(wrong_channel_msg)
 
 async def setup(bot):
     await bot.add_cog(CommandCheck(bot))
