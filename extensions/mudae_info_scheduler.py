d0f996a7995e504980e2900e8bbbf6e7b47ae4c6
Gstar <gstar973@outlook.fr>
2023-09-15 18:53:39 +0200

    commit

--- extensions/mudae_info_scheduler.py
+++ extensions/mudae_info_scheduler.py
@@ -22,13 +22,13 @@ messages = [
         "title": "Quel est l'ordre optimal pour obtenir les badges ?",
         "description": "- Pour économiser plus de 10 000 kakeras, l'ordre recommandé pour obtenir les badges est : Bronze 2, Silver 2, Gold 2 (dans l'ordre que vous préférez), suivi de Ruby 4, puis ce que vous voulez ensuite.",
         "emoji": ":camera_with_flash:",
-        "command_desc": "[Ordre recommandé pour obtenir les badges](https://www.zupimages.net/up/23/37/3ch5.png)"
+        "command_desc": "[Ordre recommandé pour obtenir les badges](https://www.zupimages.net/up/23/37/ip79.png)"
     },
     {
         "title": "Comment obtenir le kakera arc-en-ciel ?",
-        "description": "- Le **kakera arc-en-ciel** :kakera_rainbow:1146568126465577020 apparaît aléatoirement lors des tirages de certains personnages déjà mariés. La probabilité est de 0.044%.",
-        "command_desc": ":camera_with_flash: [Apparition et récupération d'un kakera arc-en-ciel](https://image.noelshack.com/fichiers/2023/35/7/1693706863-kakeraarcenciel.png)\n"
-                    ":camera_with_flash: [Tableau des probabilités d'apparition des kakeras](https://image.noelshack.com/fichiers/2023/35/7/1693707489-dropratekakera.png)"
+        "description": "- Le **kakera arc-en-ciel** <:kakera_rainbow:1146568126465577020> apparaît aléatoirement lors des tirages de certains personnages déjà mariés. La probabilité est de 0.044%.",
+        "command_desc": ":camera_with_flash: [Apparition et récupération d'un kakera arc-en-ciel](https://image.noelshack.com/fichiers/2023/35/7/1693706863-kakeraarcenciel.png>)\n"
+                    ":camera_with_flash: [Tableau des probabilités d'apparition des kakeras](<https://image.noelshack.com/fichiers/2023/35/7/1693707489-dropratekakera.png)"
     },
     {
         "title": "Que faire si j'obtiens un kakera arc-en-ciel ?",
@@ -43,7 +43,7 @@ messages = [
     },
     {
         "title": "Où sont annoncés les gagnants et peuvent-ils gagner plusieurs fois ?",
-        "description": "- Les noms des gagnants sont publiés dans le salon #1146566186012778556. Un même joueur peut y apparaître plusieurs fois s'il a remporté plusieurs victoires, c'est-à-dire qu'il a fait apparaître et a récupéré le **kakera arc-en-ciel** :kakera_rainbow:1146568126465577020 à plusieurs reprises.",
+        "description": "- Les noms des gagnants sont publiés dans le salon <#1146566186012778556>. Un même joueur peut y apparaître plusieurs fois s'il a remporté plusieurs victoires, c'est-à-dire qu'il a fait apparaître et a récupéré le **kakera arc-en-ciel** <:kakera_rainbow:1146568126465577020> à plusieurs reprises.",
         "command_desc": ":camera_with_flash: [Salon des accomplissements](https://image.noelshack.com/fichiers/2023/35/7/1693706863-accomplissement.png)"
     },
     {
@@ -53,7 +53,7 @@ messages = [
     },
     {
         "title": "Comment débuter avec Mudae ?",
-        "description": "- Pour vous initier, lancez le tutoriel en tapant `$tuto` dans le salon #1146892017402662912. Après avoir terminé une sous-étape, pensez à rafraîchir le tutoriel en tapant à nouveau `$tuto` pour passer à la page suivante.",
+        "description": "- Pour vous initier, lancez le tutoriel en tapant `$tuto` dans le salon <#1146892017402662912>. Après avoir terminé une sous-étape, pensez à rafraîchir le tutoriel en tapant à nouveau `$tuto` pour passer à la page suivante.",
         "command_desc": ":camera_with_flash: [Utilisation de la commande `$tuto`](https://image.noelshack.com/fichiers/2023/35/7/1693706882-tuto.png)"
     },
     {
@@ -63,29 +63,29 @@ messages = [
     },
     {
         "title": "Qu'est-ce que le salon de paramétrage ?",
-        "description": "- Le salon #1146892017402662912 est votre tableau de bord pour ajuster les paramètres du jeu et gérer les kakeras.",
+        "description": "- Le salon <#1146892017402662912> est votre tableau de bord pour ajuster les paramètres du jeu et gérer les kakeras.",
         "command_desc": ":camera_with_flash: [Salon mudae-settings](https://image.noelshack.com/fichiers/2023/35/7/1693706903-salonseetings.png)"
     },
     {
         "title": "Comment gagner des kakeras ?",
-        "description": "- Utilisez `$divorce` et `$dk` dans #1146892017402662912, et réagissez aux kakeras aléatoires dans #1146542491290579094.",
+        "description": "- Utilisez `$divorce` et `$dk` dans <#1146892017402662912>, et réagissez aux kakeras aléatoires dans <#1146542491290579094>.",
         "command_desc": ":clapper: [Utilisation de la commande `$divorce`](https://i.imgur.com/Ot2dJQQ.gif)\n"
                     ":clapper: [Utilisation de la commande `$dk`](https://i.imgur.com/WWhfuSA.gif)\n"
                     ":clapper: [Apparition et récupération d'un kakera](https://i.imgur.com/7lSfm9I.gif)"
     },
     {
         "title": "Comment optimiser mes tirages ?",
-        "description": "- Utilisez la commande `$disable` dans #1146892017402662912 pour désactiver certaines séries et ainsi augmenter vos chances d'obtenir des personnages spécifiques. Pour plus d'informations sur les séries à désactiver, consultez [ce tableau](https://docs.google.com/spreadsheets/d/1uSn_E6bdzkQzB-tmDii1Es21tz9hvbvEckc6pGOno40/edit). Notez que l'utilisation de `$wish` réactivera un personnage d'une série que vous aviez précédemment désactivée.",
+        "description": "- Utilisez la commande `$disable` dans <#1146892017402662912> pour désactiver certaines séries et ainsi augmenter vos chances d'obtenir des personnages spécifiques. Pour plus d'informations sur les séries à désactiver, consultez [ce tableau](https://docs.google.com/spreadsheets/d/1uSn_E6bdzkQzB-tmDii1Es21tz9hvbvEckc6pGOno40/edit). Notez que l'utilisation de `$wish` réactivera un personnage d'une série que vous aviez précédemment désactivée.",
         "command_desc": ":clapper: [Utilisation de la commande `$disable`](https://i.imgur.com/Vtc6dw1.gif)"
     },
     {
         "title": "Où trouver des astuces et la liste des commandes ?",
-        "description": "- Consultez le salon #1122920806155042997 pour des astuces et des commandes utiles.",
+        "description": "- Consultez le salon <#1122920806155042997> pour des astuces et des commandes utiles.",
         "command_desc": ":camera_with_flash: [Salon mudae-commands](https://image.noelshack.com/fichiers/2023/35/7/1693706863-saloncommands.png)"
     },
     {
         "title": "Où poser des questions supplémentaires ?",
-        "description": "- Si vous avez des questions supplémentaires, posez-les dans le salon #1131562652913647657.",
+        "description": "- Si vous avez des questions supplémentaires, posez-les dans le salon <#1131562652913647657>.",
         "command_desc": ":camera_with_flash: [Salon mudae-help](https://image.noelshack.com/fichiers/2023/35/7/1693706903-salonhelp.png)"
     }
 ]
@@ -96,7 +96,7 @@ class MudaeInfoScheduler(commands.Cog):
         self.last_message_index = None
         self.shuffled_messages = messages.copy()
         shuffle(self.shuffled_messages)
-        self.delay_minutes = 1
+        self.delay_minutes = 60
         self.bot.loop.create_task(self.send_periodic_message())
 
     @commands.Cog.listener()
