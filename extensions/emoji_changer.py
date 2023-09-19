8b15b94e1fdfadb4c15ada20d14ca6d2b4ab59e7
Gstar <gstar973@outlook.fr>
2023-09-19 12:14:03 +0200

    commit

--- extensions/emoji_changer.py
+++ extensions/emoji_changer.py
@@ -10,7 +10,7 @@ class EmojiChanger(commands.Cog):
     def cog_unload(self):
         self.change_emoji.cancel()
 
-    @tasks.loop(minutes=10)
+    @tasks.loop(minutes=5)
     async def change_emoji(self):
         channel = self.bot.get_channel(1152606429400805476)
         if not channel:
