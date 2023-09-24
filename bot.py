81920e1a64bbc16cfa67fa9f4066c8fb8272d11d
Gstar <gstar973@outlook.fr>
2023-09-24 09:14:01 +0200

    commit

--- bot.py
+++ bot.py
@@ -14,17 +14,45 @@ bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    
     await bot.load_extension("extensions.inscription")
+    print("Inscription extension loaded")
+    
     await bot.load_extension("extensions.thread_creator")
+    print("Thread Creator extension loaded")
+    
     await bot.load_extension("extensions.presentation")
+    print("Presentation extension loaded")
+    
     await bot.load_extension("extensions.question")
+    print("Question extension loaded")
+    
     await bot.load_extension("extensions.command_check")
+    print("Command Check extension loaded")
+    
     await bot.load_extension("extensions.watch")
+    print("Watch extension loaded")
+    
     await bot.load_extension("extensions.mudae_info_scheduler")
-    # await bot.load_extension("extensions.faq_mudae_1")
-    # await bot.load_extension("extensions.faq_mudae_2")
+    print("Mudae Info Scheduler extension loaded")
+    
+    await bot.load_extension("extensions.role_management")
+    print("Role Management extension loaded")
+    
     await bot.load_extension("extensions.emoji_changer")
+    print("Emoji Changer extension loaded")
+    
+    # Uncomment the following lines as needed for other extensions
+    # await bot.load_extension("extensions.faq_mudae_1")
+    # print("FAQ Mudae 1 extension loaded")
+    
+    # await bot.load_extension("extensions.faq_mudae_2")
+    # print("FAQ Mudae 2 extension loaded")
+    
     # await bot.load_extension("extensions.channel_visibility_manager")
+    # print("Channel Visibility Manager extension loaded")
+    
     # await bot.load_extension("extensions.mudae_role_manager")
+    # print("Mudae Role Manager extension loaded")
 
 bot.run(TOKEN)
