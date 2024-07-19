5008271adf74388d81254ddc4a36218a8bd56a03
Gstarmix <gstar973@outlook.fr>
2024-07-19 10:56:46 +0200

    test

--- extensions/auto_repost_on_delete.py
+++ extensions/auto_repost_on_delete.py
@@ -4,12 +4,11 @@ from discord.ext import commands
 import aiohttp
 import json
 import os
-import asyncio
 
 from constants import QUESTION_CHANNEL_ID, ESTIMATION_CHANNEL_ID, ACTIVITES_ID
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 DATA_PATH = "extensions/auto_repost_on_delete_data.json"
@@ -18,69 +17,32 @@ class AutoRepostOnDelete(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.forum_channel_ids = {QUESTION_CHANNEL_ID, ESTIMATION_CHANNEL_ID, ACTIVITES_ID}
+        self.thread_to_message = {}
         self.saved_attachments = self.load_thread_data()
-        self.bot.loop.create_task(self.fetch_all_threads())
 
     def load_thread_data(self):
         if os.path.exists(DATA_PATH):
             with open(DATA_PATH, 'r') as file:
-                content = file.read().strip()
-                if content:
-                    return json.loads(content)
-                else:
-                    return {}
+                data = json.load(file)
+                self.thread_to_message = {int(thread_id): msg_id for thread_id, msg_id in data.get("thread_to_message", {}).items()}
+                return data.get("saved_attachments", {})
         return {}
 
     def save_thread_data(self):
+        data = {
+            "thread_to_message": self.thread_to_message,
+            "saved_attachments": self.saved_attachments
+        }
         with open(DATA_PATH, 'w') as file:
-            json.dump(self.saved_attachments, file)
-
-    async def get_first_message(self, thread):
-        async for message in thread.history(limit=1, oldest_first=True):
-            return message
-        return None
-
-    async def fetch_all_threads(self):
-        await self.bot.wait_until_ready()  # Assure que le bot est prêt avant de commencer
-        for channel_id in self.forum_channel_ids:
-            channel = self.bot.get_channel(channel_id)
-            if channel is None:
-                continue
-
-            try:
-                for thread in channel.threads:
-                    if str(thread.id) not in self.saved_attachments:
-                        await self.add_thread_info(thread)
-
-                async for thread in channel.archived_threads(limit=None):
-                    if str(thread.id) not in self.saved_attachments:
-                        await self.add_thread_info(thread)
-            except Exception as e:
-                await asyncio.sleep(5)
-                await self.fetch_all_threads()
-
-    async def add_thread_info(self, thread):
-        first_message = await self.get_first_message(thread)
-        if first_message:
-            attachment_paths = await self.download_and_save_attachments(first_message.attachments, thread.id)
-            self.saved_attachments[str(thread.id)] = {
-                "content": first_message.content,
-                "author_id": first_message.author.id,
-                "attachments": attachment_paths
-            }
-            self.save_thread_data()
+            json.dump(data, file)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if isinstance(message.channel, discord.Thread) and message.channel.parent_id in self.forum_channel_ids:
-            thread_id = str(message.channel.id)
-            if thread_id not in self.saved_attachments:
-                attachment_paths = await self.download_and_save_attachments(message.attachments, message.channel.id)
-                self.saved_attachments[thread_id] = {
-                    "content": message.content,
-                    "author_id": message.author.id,
-                    "attachments": attachment_paths
-                }
+            thread_id = message.channel.id
+            if thread_id not in self.thread_to_message:
+                self.thread_to_message[thread_id] = message.id
+                self.saved_attachments[message.id] = await self.download_and_save_attachments(message.attachments, thread_id)
                 self.save_thread_data()
 
     async def download_and_save_attachments(self, attachments, thread_id):
@@ -98,47 +60,34 @@ class AutoRepostOnDelete(commands.Cog):
                             with open(file_path, 'wb') as f:
                                 f.write(data)
                             attachment_paths.append(file_path)
+                        else:
+                            logger.error(f"Failed to download attachment: {attachment.url}, status code: {response.status}")
             except Exception as e:
-                pass
+                logger.error(f"Error downloading attachment: {attachment.url}, error: {e}")
         return attachment_paths
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message):
         if isinstance(message.channel, discord.Thread):
-            thread_id = str(message.channel.id)
-            if thread_id in self.saved_attachments and self.saved_attachments[thread_id]["content"] == message.content:
-                await self.repost_message(message, thread_id)
+            thread = message.channel
+            if thread.id in self.thread_to_message and self.thread_to_message[thread.id] == message.id:
+                await self.repost_message(message, thread.id)
+            elif message.id in self.saved_attachments:
+                await self.repost_message_from_data(thread.id, message.id)
 
-    @commands.Cog.listener()
-    async def on_message_edit(self, before, after):
-        if isinstance(after.channel, discord.Thread) and after.channel.parent_id in self.forum_channel_ids:
-            thread_id = str(after.channel.id)
-            if thread_id in self.saved_attachments:
-                self.saved_attachments[thread_id]["content"] = after.content
-                attachment_paths = await self.download_and_save_attachments(after.attachments, after.channel.id)
-                self.saved_attachments[thread_id]["attachments"] = attachment_paths
-                self.save_thread_data()
+    async def repost_message(self, message: discord.Message, thread_id: int):
+        user = message.author
+        content = message.content or "*No content*"
+        attachment_paths = self.saved_attachments.get(message.id, [])
 
-    @commands.Cog.listener()
-    async def on_thread_delete(self, thread):
-        if thread.parent_id in self.forum_channel_ids:
-            self.saved_attachments.pop(str(thread.id), None)
-            self.save_thread_data()
-
-    async def repost_message(self, message: discord.Message, thread_id: str):
-        user_id = self.saved_attachments[thread_id]["author_id"]
-        user = await self.bot.fetch_user(user_id)
-        content = self.saved_attachments[thread_id]["content"]
-        attachment_paths = self.saved_attachments.get(thread_id, {}).get("attachments", [])
-
-        thread = await self.bot.fetch_channel(int(thread_id))
+        thread = await self.bot.fetch_channel(thread_id)
         webhooks = await thread.parent.webhooks()
         webhook = discord.utils.find(lambda wh: wh.user == thread.guild.me, webhooks)
         if webhook is None:
             webhook = await thread.parent.create_webhook(name="AutoReposter")
 
         files = [(f"attachment://{os.path.basename(path)}", discord.File(path, filename=os.path.basename(path))) for path in attachment_paths]
-
+        
         if files:
             image_urls, discord_files = zip(*files)
             view = ImageNavigator(list(image_urls), "Le premier message de ce fil a été supprimé", content, discord.Color.red(), list(discord_files))
@@ -160,6 +109,41 @@ class AutoRepostOnDelete(commands.Cog):
             await webhook.send(
                 username=user.display_name,
                 avatar_url=user.display_avatar.url,
+                thread=thread,
+                embed=embed
+            )
+
+    async def repost_message_from_data(self, thread_id: int, message_id: int):
+        attachment_paths = self.saved_attachments.get(message_id, [])
+        thread = await self.bot.fetch_channel(thread_id)
+        webhooks = await thread.parent.webhooks()
+        webhook = discord.utils.find(lambda wh: wh.user == thread.guild.me, webhooks)
+        if webhook is None:
+            webhook = await thread.parent.create_webhook(name="AutoReposter")
+
+        files = [(f"attachment://{os.path.basename(path)}", discord.File(path, filename=os.path.basename(path))) for path in attachment_paths]
+        
+        if files:
+            image_urls, discord_files = zip(*files)
+            view = ImageNavigator(list(image_urls), "Le premier message de ce fil a été supprimé", "*Contenu non disponible*", discord.Color.red(), list(discord_files))
+            embed = view.update_embed(view.current_image)
+            await webhook.send(
+                username="Deleted User",
+                avatar_url="",
+                thread=thread,
+                embed=embed,
+                view=view,
+                file=discord_files[0]
+            )
+        else:
+            embed = discord.Embed(
+                title="Le premier message de ce fil a été supprimé",
+                description="*Contenu non disponible*",
+                color=discord.Color.red()
+            )
+            await webhook.send(
+                username="Deleted User",
+                avatar_url="",
                 thread=thread,
                 embed=embed
             )
